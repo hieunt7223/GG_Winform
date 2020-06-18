e6915abef3c9a112d9be1476ad79086918d8e243
hieunt7223 <hieunt702@gmail.com>
2020-06-18 16:47:40 +0700

    hieunt: Fix code

---
+++ GG.Component/Control/GGGroupControl.cs
@@ -0,0 +1,86 @@
+﻿using GG.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace GG.Component
+{
+    public partial class GGGroupControl : DevExpress.XtraEditors.GroupControl, IGGControl
+    {
+
+        #region Variables
+        protected String _GGDataSource;
+        protected String _GGDataMember;
+        protected String _GGFieldGroup;
+        protected String _GGFieldRelation;
+        #endregion
+
+        #region Public Properties
+        [Category("GreenGlobal")]
+        public String GGDataSource
+        {
+            get
+            {
+                return _GGDataSource;
+            }
+            set
+            {
+                _GGDataSource = value;
+            }
+        }
+
+        [Category("GreenGlobal")]
+        public String GGDataMember
+        {
+            get
+            {
+                return _GGDataMember;
+            }
+            set
+            {
+                _GGDataMember = value;
+            }
+        }
+
+        [Category("GreenGlobal")]
+        public String GGFieldGroup
+        {
+            get
+            {
+                return _GGFieldGroup;
+            }
+            set
+            {
+                _GGFieldGroup = value;
+            }
+        }
+
+        [Category("GreenGlobal")]
+        public String GGFieldRelation
+        {
+            get
+            {
+                return _GGFieldRelation;
+            }
+            set
+            {
+                _GGFieldRelation = value;
+            }
+        }
+
+        #endregion
+
+        #region CustomControl
+        List<ADConfigColumns> listConfigColumn;
+        public GGGroupControl()
+        {
+            listConfigColumn = new List<ADConfigColumns>();
+        }
+
+        public virtual void InitializeControl()
+        {
+            //
+        }
+        #endregion
+    }
+}
