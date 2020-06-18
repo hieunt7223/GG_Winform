c0ed2ea60b6d77edcd91be1e96334e4a99c73de2
hieunt7223 <hieunt702@gmail.com>
2020-06-18 13:52:00 +0700

    hieunt: Commit Code

---
+++ GG.Common/Class/Constants.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GG.Common.Class
+{
+    public class TableName
+    {
+        #region Cấu hình
+        public const string ADConfigColumnsTableName = "ADConfigColumns";
+        public const string ADConfigValuesTableName = "ADConfigValues";
+        #endregion
+
+        #region Nghiệp vụ
+        public const string KeHoachTongNguonVonHieuTableName = "KeHoachTongNguonVonHieu";
+        public const string KeHoachTongNguonVonChungHieuTableName = "KeHoachTongNguonVonChungHieu";
+        #endregion
+
+        #region Danh muc
+        public const string ChiTieuBaoCaoHieusTableName = "ChiTieuBaoCaoHieus";
+        #endregion
+
+        #region Constants of Property Names
+
+        #endregion
+    }
+
+    public class Customs
+    {
+        public const String cstDataSource = "GGDataSource";
+        public const String cstDataMember = "GGDataMember";
+        public const String cstFieldGroup = "GGFieldGroup";
+        public const String cstFieldRelation = "GGFieldRelation";
+    }
+}
---
+++ GG.Common/Class/Enums.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GG.Common.Class
+{
+    public enum Status
+    {
+        Alive,
+        Delete,
+        Dummy
+    }
+
+    public enum ADConfigValueColumn
+    {
+        ADConfigKeyValue,
+        ADConfigText,
+        ADConfigKeyGroup
+    }
+
+    public enum ButtonAction
+    { 
+        btnNew,
+        btnNewByUI,
+        btnSave,
+        btnCancel,
+        btnDelete,
+        btnEdit,
+    }
+}
