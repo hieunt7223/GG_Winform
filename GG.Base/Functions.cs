bfaabae10fbdb4b58b9cc0c77bc90983619f473e
hieunt7223 <hieunt702@gmail.com>
2020-06-20 15:40:18 +0700

    hieunt: fix code

---
+++ GG.Base/Functions.cs
@@ -0,0 +1,18 @@
+﻿using DevExpress.XtraEditors;
+using System.Windows.Forms;
+
+namespace GG.Base
+{
+    public class Functions
+    {
+        public static void ShowMessage(string mess)
+        {
+            XtraMessageBox.Show(mess, BaseLocalizedResources.Notification, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public static DialogResult ShowMessageYesNo(string mess)
+        {
+            return XtraMessageBox.Show(mess, BaseLocalizedResources.Notification, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+        }
+    }
+}
---
+++ GG.Studio/MainForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using GG.Base;
+using GG.Entity;
+using GG.Repository;
+
+namespace GG.Studio
+{
+    public partial class MainForm : XtraForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void smi_DesignUI_Click(object sender, EventArgs e)
+        {
+            List<ADConfigColumns> test = ADConfigColumnsRepository.GetAllData();
+            //if (checkPannel() == false) return;
+            //frmCreateUI frm = new frmCreateUI();
+            //if (frm != null)
+            //{
+            //    pn_view.Controls.Clear();
+            //    pn_view.Controls.Add(frm);
+            //    frm.Dock = DockStyle.Fill;
+            //    frm.Show();
+            //}
+        }
+
+        private void smi_GenerateEntity_Click(object sender, EventArgs e)
+        {
+            //if (checkPannel() == false) return;
+            //frmGenerateEntity frm = new frmGenerateEntity();
+            //if (frm != null)
+            //{
+            //    pn_view.Controls.Clear();
+            //    pn_view.Controls.Add(frm);
+            //    frm.Dock = DockStyle.Fill;
+            //    frm.Show();
+            //}
+        }
+
+        private bool checkPannel()
+        {
+            bool check = true;
+            if (pn_view.Controls.Count > 0)
+            {
+                DialogResult result = Functions.ShowMessageYesNo("Bạn có chắc chắn muốn mở màn hình khác không?");
+                if (result == DialogResult.Yes)
+                {
+                    check = true;
+                }
+                else
+                {
+                    check = false;
+                }
+            }
+            return check;
+        }
+    }
+}
