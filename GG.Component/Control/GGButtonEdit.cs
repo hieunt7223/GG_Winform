0964d83294d624a634b18f2abc71596bedc6d708
hieunt7223 <hieunt702@gmail.com>
2020-06-20 15:17:04 +0700

    hieunt: Fix code

---
+++ GG.Component/Control/GGButtonEdit.cs
@@ -0,0 +1,84 @@
+﻿using GG.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace GG.Component
+{
+    public partial class GGButtonEdit : DevExpress.XtraEditors.ButtonEdit, IGGControl
+    {
+        #region Variables
+        protected String _GGDataSource;
+        protected String _GGDataMember;
+        protected String _GGFieldGroup;
+        protected String _GGFieldRelation;
+        #endregion
+
+        #region Public Properties
+        [Category("GreenGlobal")]
+        public String GGDataSource
+        {
+            get
+            {
+                return _GGDataSource;
+            }
+            set
+            {
+                _GGDataSource = value;
+            }
+        }
+
+        [Category("GreenGlobal")]
+        public String GGDataMember
+        {
+            get
+            {
+                return _GGDataMember;
+            }
+            set
+            {
+                _GGDataMember = value;
+            }
+        }
+
+        [Category("GreenGlobal")]
+        public String GGFieldGroup
+        {
+            get
+            {
+                return _GGFieldGroup;
+            }
+            set
+            {
+                _GGFieldGroup = value;
+            }
+        }
+
+        [Category("GreenGlobal")]
+        public String GGFieldRelation
+        {
+            get
+            {
+                return _GGFieldRelation;
+            }
+            set
+            {
+                _GGFieldRelation = value;
+            }
+        }
+
+        #endregion
+
+        #region CustomControl
+        List<ADConfigColumns> listConfigColumn;
+        public GGButtonEdit()
+        {
+            listConfigColumn = new List<ADConfigColumns>();          
+        }
+
+        public virtual void InitializeControl()
+        {
+        }
+        #endregion
+    }
+}
